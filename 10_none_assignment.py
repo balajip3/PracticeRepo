bb922b5e8cc2f11d64f1e345e3c9a4d0f7863100
Balaji Polipogu <polipogubalaji3@gmail.com>
2026-08-26 12:55:55 +0530

    Implement None datatype assignments and checks

--- 10_none_assignment.py
+++ 10_none_assignment.py
@@ -19,38 +19,81 @@ print("-" * 50)
 print("Question 1: Create a variable and assign None to it")
 # Your code here
 
+a = None 
+
+print(a)
+
 # Question 2: Check if None equals None
 print("\nQuestion 2: Check if None equals None")
 # Your code here
+
+print(None == None)
 
 # Question 3: Check if None is None
 print("\nQuestion 3: Check if None is None")
 # Your code here
 
+print(None is None)
+
+
 # Question 4: Check if 0 is None
 print("\nQuestion 4: Check if 0 is None")
 # Your code here
+
+a = 0 
+
+print(a is None)
 
 # Question 5: Check if empty string is None
 print("\nQuestion 5: Check if empty string is None")
 # Your code here
 
+a = ""
+
+print(a is None)
+
 # Question 6: Check if False is None
 print("\nQuestion 6: Check if False is None")
 # Your code here
+
+a = False
+
+print(False is None)
+
+
 
 # Question 7: Check if empty list is None
 print("\nQuestion 7: Check if empty list is None")
 # Your code here
 
+a = []
+
+print(a is None)
+
+
 # Question 8: Check if empty dictionary is None
 print("\nQuestion 8: Check if empty dictionary is None")
 # Your code here
+
+a = {}
+
+print(a is None)
 
 # Question 9: Check if empty tuple is None
 print("\nQuestion 9: Check if empty tuple is None")
 # Your code here
 
+a = ()
+
+print(a is None)
+
+
+
 # Question 10: Check if empty set is None
 print("\nQuestion 10: Check if empty set is None")
 # Your code here 
+
+a = set()
+
+print(a is None)
+
