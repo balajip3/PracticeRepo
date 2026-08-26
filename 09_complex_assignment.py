c98eaebb2c5ae0009589e8865b4b91345f1f1c02
Balaji Polipogu <polipogubalaji3@gmail.com>
2026-08-26 12:41:32 +0530

    Implement complex number operations and examples

--- 09_complex_assignment.py
+++ 09_complex_assignment.py
@@ -20,39 +20,95 @@ print("-" * 50)
 # Question 1: Create complex number 5 + 3j
 print("Question 1: Create complex number 5 + 3j")
 # Your code here
+a = 5 + 3j
+print(a)
 
 # Question 2: Find the real part of complex number 7 - 2j
 print("\nQuestion 2: Find the real part of complex number 7 - 2j")
 # Your code here
 
+a = 7 - 2j 
+
+print(a.real)
+
 # Question 3: Find the imaginary part of complex number 4 + 6j
 print("\nQuestion 3: Find the imaginary part of complex number 4 + 6j")
 # Your code here
+
+a = 4 + 6j 
+
+print(a.imag)
+
 
 # Question 4: Add two complex numbers: (3 + 2j) and (1 + 4j)
 print("\nQuestion 4: Add two complex numbers: (3 + 2j) and (1 + 4j)")
 # Your code here
 
+a = (3 + 2j)
+b = (1 + 4j)
+
+c = a + b
+
+print(c)
+
 # Question 5: Multiply two complex numbers: (2 + 3j) and (1 + 2j)
 print("\nQuestion 5: Multiply two complex numbers: (2 + 3j) and (1 + 2j)")
 # Your code here
+
+a = (2 + 3j)
+b = (1 + 2j)
+
+c = a * b
+
+print(c)
 
 # Question 6: Find the magnitude of complex number 6 + 8j
 print("\nQuestion 6: Find the magnitude of complex number 6 + 8j")
 # Your code here
 
+a = 6 + 8j
+
+print(abs(a))
+
 # Question 7: Find the conjugate of complex number 5 - 7j
 print("\nQuestion 7: Find the conjugate of complex number 5 - 7j")
 # Your code here
+
+a = 5 - 7j
+
+print(a.conjugate())
 
 # Question 8: Subtract complex numbers: (10 + 5j) - (3 + 2j)
 print("\nQuestion 8: Subtract complex numbers: (10 + 5j) - (3 + 2j)")
 # Your code here
 
+a = (10 - 5j) 
+b = (3 + 2j)
+
+c = a - b 
+
+print(c)
+
 # Question 9: Divide complex numbers: (8 + 6j) / (2 + 1j)
 print("\nQuestion 9: Divide complex numbers: (8 + 6j) / (2 + 1j)")
 # Your code here
 
+a = (8 + 6j)
+
+b = (2 + 1j)
+
+c = a / b 
+
+print(c)
+
+
 # Question 10: Find the phase angle of complex number 1 + 1j
 print("\nQuestion 10: Find the phase angle of complex number 1 + 1j")
 # Your code here 
+
+import cmath 
+
+a = 1 + 1j
+
+print(cmath.phase(a))
+
